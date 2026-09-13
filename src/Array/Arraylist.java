58eaf9f995694456c2e3d103f4af8273a9a09886
[SatyamAgrawal] <satyamagrawal1101@gmail.com>
2026-09-14 01:40:46 +0530

    ForEachLoop

--- src/Array/Arraylist.java
+++ src/Array/Arraylist.java
@@ -1,4 +1,29 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Arraylist {
+    static void main() {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(23);
+        arr.add(5);
+        arr.add(54);
+        arr.add(41);
+        arr.add(50);
+        System.out.println(arr.get(1));
+        arr.set(3,12); //arr[1]=90
+
+        System.out.println(arr);
+
+        for (Integer integer : arr) {
+            System.out.print(integer + " ");
+        }
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+        Collections.reverse(arr);
+        System.out.println(arr);
+    }
 }
--- src/Array/ForEachLoop.java
+++ src/Array/ForEachLoop.java
@@ -1,4 +1,10 @@
 package Array;
 
 public class ForEachLoop {
+    public static void main(String[] args){
+        int[] arr={12,45,86,76,43,82};
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+    }
 }
